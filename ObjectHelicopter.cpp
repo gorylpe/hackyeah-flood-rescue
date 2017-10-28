2d731796760909d4fd47f7f34406dde36f093945
Luz4r <piorek.r@gmail.com>
2017-10-28 23:24:33 +0200

    adding ObjectHelicopter

---
+++ ObjectHelicopter.cpp
@@ -0,0 +1,8 @@
+//
+// Created by pietrk01 on 10/28/2017.
+//
+
+#include "ObjectHelicopter.h"
+
+ObjectHelicopter::ObjectHelicopter(int _x, int _y)
+:Object(OBJECTTYPE::HELICOPTER, _x, _y) {}
---
+++ ObjectHelicopter.h
@@ -0,0 +1,17 @@
+//
+// Created by pietrk01 on 10/28/2017.
+//
+
+#ifndef GRA_OBJECTHELICOPTER_H
+#define GRA_OBJECTHELICOPTER_H
+
+#include "Object.h"
+
+
+class ObjectHelicopter : public Object {
+public:
+    ObjectHelicopter(int _x, int _y);
+};
+
+
+#endif //GRA_OBJECTHELICOPTER_H
