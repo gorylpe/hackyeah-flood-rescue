6744b4143359fc8459de99b3e290d62d149892aa
Piotr <gorylp71@gmail.com>
2017-10-28 19:32:26 +0200

    tile sandbags

--- Tile.cpp
+++ Tile.cpp
@@ -3,3 +3,12 @@
 //
 
 #include "Tile.h"
+
+Tile::Tile(Tile::TILETYPE _tileType, int _height)
+    :tileType(_tileType)
+    ,height(_height)
+    ,sandbagsDown(false)
+    ,sandbagsUp(false)
+    ,sandbagsLeft(false)
+    ,sandbagsRight(false)
+{}
