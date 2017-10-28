0fb4be9b12fadc82aa6cf4ed9d407ae2ac5f174f
Luz4r <piorek.r@gmail.com>
2017-10-28 20:32:36 +0200

    adding class UIElement

---
+++ UIElement.h
@@ -0,0 +1,16 @@
+//
+// Created by pietrk01 on 10/28/2017.
+//
+
+#include <vector>
+#ifndef GRA_UIELEMENT_H
+#define GRA_UIELEMENT_H
+
+
+class UIElement {
+
+    int x, y, width, height;
+};
+
+
+#endif //GRA_UIELEMENT_H
