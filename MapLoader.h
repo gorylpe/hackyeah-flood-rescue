e765d1de0a5bc793971d9d1d0134c9a3f62db055
Piotr <gorylp71@gmail.com>
2017-10-28 16:48:04 +0200

    map loader initial

---
+++ MapLoader.h
@@ -0,0 +1,17 @@
+//
+// Created by Piotr on 28.10.2017.
+//
+
+#ifndef GRA_MAPLOADER_H
+#define GRA_MAPLOADER_H
+
+#include <string>
+#include "Map.h"
+
+class MapLoader {
+public:
+    Map* loadMap(std::string filename);
+};
+
+
+#endif //GRA_MAPLOADER_H
