5bc55b9b13a8aa85cb2baf7b577288be78185e53
Luz4r <piorek.r@gmail.com>
2017-10-28 15:36:43 +0200

    expanding GameManager

--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
-#include <iostream>
-#include <SFML/Graphics.hpp>
-#include <cmath>
 #include "GameManager.h"
 
 int main() {
-   GameManager::getSingleton().mainLoop();
+    GameManager::getSingleton().mainLoop();
     return 0;
 }
