631a8baff8f2da2bc171c896e8569de375cb4cc1
Piotr <gorylp71@gmail.com>
2017-10-28 23:49:39 +0200

    drawing sandbags

--- DrawingManager.cpp
+++ DrawingManager.cpp
@@ -46,6 +46,11 @@ void DrawingManager::loadSprites(){
     textureMap->push_back(texture);
 
     texture = new sf::Texture();
+    texture->loadFromFile("sprites/firestation.png");
+    texture->setSmooth(true);
+    textureMap->push_back(texture);
+
+    texture = new sf::Texture();
     texture->loadFromFile("sprites/firetruck.png");
     texture->setSmooth(true);
     textureMap->push_back(texture);
@@ -66,30 +71,37 @@ void DrawingManager::draw(Map *map, std::vector<Object *>* objectsArray) {
             window->draw(sprite);
 
             if(tile->getSandbagsUp()){
+                sf::Sprite sprite;
                 sprite.setTexture(*textureMap->at(DrawableObject::TEXTURE::SANDBAGS));
-                sprite.setOrigin(sprite.getLocalBounds().width / 2, sprite.getLocalBounds().height / 2);
+                sprite.setOrigin(sprite.getLocalBounds().width, sprite.getLocalBounds().height);
+                sprite.setRotation(180);
                 sprite.setScale(1.0 * tileWidth / sprite.getLocalBounds().width, 1.0 * tileHeight / sprite.getLocalBounds().height);
+                sprite.setPosition(x * tileWidth, y * tileHeight);
                 window->draw(sprite);
             }
             if(tile->getSandbagsDown()){
+                sf::Sprite sprite;
                 sprite.setTexture(*textureMap->at(DrawableObject::TEXTURE::SANDBAGS));
-                sprite.setOrigin(sprite.getLocalBounds().width / 2, sprite.getLocalBounds().height / 2);
                 sprite.setScale(1.0 * tileWidth / sprite.getLocalBounds().width, 1.0 * tileHeight / sprite.getLocalBounds().height);
-                sprite.setRotation(180);
+                sprite.setPosition(x * tileWidth, y * tileHeight);
                 window->draw(sprite);
             }
             if(tile->getSandbagsLeft()){
+                sf::Sprite sprite;
                 sprite.setTexture(*textureMap->at(DrawableObject::TEXTURE::SANDBAGS));
-                sprite.setOrigin(sprite.getLocalBounds().width / 2, sprite.getLocalBounds().height / 2);
+                sprite.setOrigin(0, sprite.getLocalBounds().height);
                 sprite.setScale(1.0 * tileWidth / sprite.getLocalBounds().width, 1.0 * tileHeight / sprite.getLocalBounds().height);
                 sprite.setRotation(90);
+                sprite.setPosition(x * tileWidth, y * tileHeight);
                 window->draw(sprite);
             }
-            if(tile->getSandbagsUp()){
+            if(tile->getSandbagsRight()){
+                sf::Sprite sprite;
                 sprite.setTexture(*textureMap->at(DrawableObject::TEXTURE::SANDBAGS));
-                sprite.setOrigin(sprite.getLocalBounds().width / 2, sprite.getLocalBounds().height / 2);
+                sprite.setOrigin(sprite.getLocalBounds().width, 0);
                 sprite.setScale(1.0 * tileWidth / sprite.getLocalBounds().width, 1.0 * tileHeight / sprite.getLocalBounds().height);
                 sprite.setRotation(270);
+                sprite.setPosition(x * tileWidth, y * tileHeight);
                 window->draw(sprite);
             }
 
