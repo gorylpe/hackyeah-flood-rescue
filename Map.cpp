55e30635c2461b9039674ba84dec31cf472fbb29
Luz4r <piorek.r@gmail.com>
2017-10-28 22:25:40 +0200

    fixing update map

--- Map.cpp
+++ Map.cpp
@@ -2,6 +2,7 @@
 // Created by CLion on 28.10.2017.
 //
 
+#include <cstring>
 #include "Map.h"
 Map::Map(int _width, int _height):
         width(_width),
@@ -28,30 +29,46 @@ int Map::getHeight() {
 }
 
 void Map::update() {
-  for(int i = 0; i < width; i++){
-     for(int j = 0; j < height; j++){
-         Tile* water = getTile(i, j);
-         if(water->getTexture() == DrawableObject::TEXTURE::WATER){
-             if(i > 0){
-                 Tile* notWater = getTile(i-1, j);
-                 changingToWater(water, notWater);
-                 }
-             if(j > 0){
-                 Tile* notWater = getTile(i, j-1);
-                 changingToWater(water, notWater);
-             }
-             if(i < width-1){
-                Tile* notWater = getTile(i+1, j);
-                 changingToWater(water, notWater);
-             }
-             if(j < height-1){
-                 Tile* notWater = getTile(i, j+1);
-                 changingToWater(water, notWater);
-             }
-             }
-         }
-     }
- }
+    Tile*** copy = new Tile**[width];
+    for (int i = 0; i < width; i++) {
+        copy[i] = new Tile*[height];
+        for(int j = 0; j < height; ++j){
+            copy[i][j] = new Tile();
+            copy[i][j]->setHeight(tiles[i][j]->getHeight());
+            copy[i][j]->setTexture(tiles[i][j]->getTexture());
+        }
+    }
+    for(int i = 0; i < width; i++){
+        for(int j = 0; j < height; j++){
+            Tile* water = copy[i][j];
+            if(water->getTexture() == DrawableObject::TEXTURE::WATER){
+                if(i > 0){
+                    Tile* notWater = getTile(i-1, j);
+                    changingToWater(water, notWater);
+                }
+                if(j > 0){
+                    Tile* notWater = getTile(i, j-1);
+                    changingToWater(water, notWater);
+                }
+                if(i < width-1){
+                    Tile* notWater = getTile(i+1, j);
+                    changingToWater(water, notWater);
+                }
+                if(j < height-1){
+                    Tile* notWater = getTile(i, j+1);
+                    changingToWater(water, notWater);
+                }
+            }
+        }
+    }
+    for (int i = 0; i < width; i++) {
+        for(int j = 0; j < height; ++j){
+            delete copy[i][j];
+        }
+        delete copy[i];
+    }
+    delete copy;
+}
 
 void Map::changingToWater(Tile* water, Tile* notWater) {
     if (notWater->getTexture() != DrawableObject::TEXTURE::WATER) {
