f3f8837738aeb54cf3609e310759e624784d47de
LiquidLemon <liquidlemon@protonmail.com>
2017-10-29 04:55:31 +0100

    Add font size to buttons

--- UIButton.h
+++ UIButton.h
@@ -10,10 +10,12 @@
 
 class UIButton : public UIElement{
     std::string text;
+    int fontSize;
 public:
     UIButton(int _x, int _y, int _width, int _height, std::string text);
 
     void draw(sf::RenderWindow *window);
+    void setFontSize(int size) { fontSize = size; }
 };
 
 
